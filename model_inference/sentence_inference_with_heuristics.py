248b1329db0c2f478401d69c5fd12224ca05ff21
Dongwei <jdw19930725@gmail.com>
2024-05-11 21:48:37 +0000

    update heuristic code

--- model_inference/sentence_inference_with_heuristics.py
+++ model_inference/sentence_inference_with_heuristics.py
@@ -68,7 +68,8 @@ data_list = list(ds['test'])
 
 train_data_list = list(ds['train'])
 np.random.seed(14)
-rand_list_from_train = np.random.choice(train_data_list, 10, replace=False)
+debug = True
+rand_list_from_train = np.random.choice(train_data_list, 9, replace=False)
 for data in rand_list_from_train:
     l = []
     d = {"role": "user", "content": "Question: " + data['question'] + "\nAnswer:"}
@@ -89,7 +90,7 @@ for data in rand_list_from_train:
     messages_gsm8k.extend(l)
 
 
-async def get_heuristic(heuristic, previous_list, world_model):
+async def get_heuristic(heuristic, previous_list, world_model, answer=None):
     if heuristic == "gpt4_world":
         probs_list = []
         for temp_previous in previous_list:
@@ -124,9 +125,9 @@ async def get_heuristic(heuristic, previous_list, world_model):
         # print("Chosen world response: " + response_list[np.argmax(probs_list)])
         
         argmax_prob = np.argmax(probs_list)
-        return argmax_prob
+        return argmax_prob, None
     elif heuristic == "random":
-        return int(random.randint(0, len(previous_list) - 1))
+        return 0, None
     elif heuristic == "llama3_world":
         probs_list = []
         for temp_previous in previous_list:
@@ -136,14 +137,13 @@ async def get_heuristic(heuristic, previous_list, world_model):
                     "content": temp_previous
                 }
             ]
-            url = 'http://c008:1236/v1/chat/completions'
+            url = 'http://c013:1236/v1/chat/completions'
             content = {
                 "model": "meta-llama/Meta-Llama-3-8B-Instruct",
                 "messages": message,
                 "max_tokens": 1000,
                 "temperature": 0.0,
                 "stop_token_ids": [128001, 128009],
-                "logprobs": True,
             }
             headers = {
                 "Content-Type": "application/json"
@@ -169,7 +169,7 @@ async def get_heuristic(heuristic, previous_list, world_model):
             except:
                 reward = np.random.randint(0, 4)
             probs_list.append(reward)
-        return np.argmax(probs_list)
+        return np.argmax(probs_list), None
     elif heuristic == "world_model_perplexity":
         probs_list = []
         for temp_previous in previous_list:
@@ -179,14 +179,15 @@ async def get_heuristic(heuristic, previous_list, world_model):
                     "content": temp_previous
                 }
             ]
-            url = 'http://c002:1236/v1/chat/completions'
+            url = 'http://c003:1235/v1/chat/completions'
             content = {
-                "model": "meta-llama/Meta-Llama-3-8B-Instruct",
+                "model": world_model,
                 "messages": message,
                 "max_tokens": 1000,
                 "temperature": 0.0,
                 "stop_token_ids": [128001, 128009],
                 "logprobs": True,
+                "top_logprobs": 1,
             }
             headers = {
                 "Content-Type": "application/json"
@@ -202,39 +203,95 @@ async def get_heuristic(heuristic, previous_list, world_model):
                     except:
                         print("Error in calling remote world model")
                         break
-            
-            # print("gpt-4 output: ", world_response['choices'][0]['message']['content'])
-            try:
-                if "Reward:" in world_response['choices'][0]['message']['content']:
-                    reward = int(world_response['choices'][0]['message']['content'].split("Reward:")[1].strip())
-                else:
-                    reward = int(world_response['choices'][0]['message']['content'].content.strip())
-            except:
-                reward = np.random.randint(0, 4)
-            probs_list.append(reward)
-        return np.minimum(probs_list)
+            if debug:
+                print("gpt-4 output: ", world_response['choices'][0]['message']['content'])
+            prob = sum(world_response['choices'][0]['logprobs']['token_logprobs']) / len(world_response['choices'][0]['logprobs']['token_logprobs'])
+            probs_list.append(prob)
+        return np.argmax(probs_list), None
+    elif heuristic == "world_model_content":
+        probs_list = []
+        world_response_list = []
+        for temp_previous in previous_list:
+            message = messages_start_world_model + [
+                {
+                    "role": "user",
+                    "content": temp_previous
+                }
+            ]
+            url = 'http://c003:1235/v1/chat/completions'
+            content = {
+                "model": world_model,
+                "messages": message,
+                "max_tokens": 1000,
+                "temperature": 0.0,
+                "stop_token_ids": [128001, 128009],
+                "logprobs": True,
+                "top_logprobs": 1,
+            }
+            headers = {
+                "Content-Type": "application/json"
+            }
         
+            session_timeout = aiohttp.ClientTimeout(total=60000,sock_connect=6000,sock_read=6000)
+        
+            async with aiohttp.ClientSession(timeout=session_timeout) as session:
+                async with session.post(url, headers=headers, json=content) as world_response:
+                    try:
+                        world_response.raise_for_status()
+                        world_response = await world_response.json()
+                    except:
+                        print("Error in calling remote world model")
+                        break
+            if debug:
+                print("gpt-4 output: ", world_response['choices'][0]['message']['content'])
+            prob = sum(world_response['choices'][0]['logprobs']['token_logprobs']) / len(world_response['choices'][0]['logprobs']['token_logprobs'])
+            probs_list.append(prob)
+            world_response_list.append(world_response['choices'][0]['message']['content'])
+        return np.argmax(probs_list), world_response_list[int(np.argmax(probs_list))]
+    else:
+        print("Heuristic not implemented!")
+        exit()
     
-async def get_response(data, pbar: tqdm, heuristic: str, agent_model: str, world_model: str):
+async def get_response(data, pbar: tqdm, heuristic: str, agent_model: str, world_model: str, agent_url: str):
     previous = "Question: " + data['question'] + "\nAnswer:"
-    prediction = ""
+    prediction, response_list = "", [""]
     normalized_answer, normalized_prediction = "", ""
     answer = data['answer'].split("####")[1].strip()
     for trail in range(15):
+        previous_list = []
+        # for first step, the content in response list is empty, so the previous is the question
+        for response in response_list:
+            # if not response.endswith('.'):
+            #     response = response + '.'
+            if "####" in response:
+                response = response.replace("####", "The answer is:")
+            previous = previous + ' ' + response
+            previous_list.append(previous)
+        
+        chosen_previous, chosen_world_model_response = await get_heuristic(heuristic, previous_list, world_model)
+        previous = previous_list[chosen_previous]
+        
+        # special case for when the heuristic is ''world_model_content'', the rationales are used to help with the generation of next action
+        if chosen_world_model_response is not None and chosen_world_model_response.strip() != "We are ready to output the final answer":
+            previous += " " + chosen_world_model_response
+        if debug:
+            print("Chosen response: " + str(chosen_previous))
+        
         new_messages = messages_gsm8k.copy()
         new_messages.append({
             "role": "user",
             "content": previous
         })
-        url = 'http://c009:1236/v1/chat/completions'
+        url = 'http://' + agent_url + '/v1/chat/completions'
         content = {
             "model": agent_model,
             "messages": new_messages,
             "max_tokens": 1000,
-            "temperature": 0.9,
+            "temperature": 0.3,
             "stop_token_ids": [128001, 128009],
             "best_of": 3,
-            "n": 3
+            "n": 3,
+            "seed": 14,
         }
         headers = {
             "Content-Type": "application/json"
@@ -255,20 +312,10 @@ async def get_response(data, pbar: tqdm, heuristic: str, agent_model: str, world
             response = output['message']['content']
             response_list.append(response)
         
-        # print("Previous: " + previous)
-        # print("Partial response: " + response)
-
-        previous_list = []
-        for response in response_list:
-            if not response.endswith('.'):
-                response = response + '.'
-            if "####" in response:
-                response = response.replace("####", "The answer is:")
-            previous = previous + ' ' + response
-            previous_list.append(previous)
-        
-        chosen_previous = await get_heuristic(heuristic, previous_list, world_model)
-        previous = previous_list[chosen_previous]
+        if debug:
+            print("Previous: " + previous.split("Answer:")[1].strip())
+            for response in response_list:
+                print("Partial response: " + response)
         
         # update result
         async with asyncio.Lock():
@@ -310,13 +357,13 @@ async def get_response(data, pbar: tqdm, heuristic: str, agent_model: str, world
     pbar.update(1)
     return d
 
-def apply_async(data_list, heuristic, agent_model, world_model):
+def apply_async(data_list, heuristic, agent_model, world_model, agent_url):
     pbar = tqdm(total=len(data_list))
     loop = asyncio.get_event_loop()
     if loop.is_closed():
         asyncio.set_event_loop(asyncio.new_event_loop())
         loop = asyncio.get_event_loop()
-    tasks = [loop.create_task(get_response(data, pbar, heuristic, agent_model, world_model)) for data in data_list]
+    tasks = [loop.create_task(get_response(data, pbar, heuristic, agent_model, world_model, agent_url)) for data in data_list]
     result = loop.run_until_complete(asyncio.gather(*tasks))
     loop.close()
     return result
@@ -328,15 +375,47 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--heuristic", type=str, default="random", help="Heuristic to use for selecting the next response")
     parser.add_argument("--agent_model", type=str, default="meta-llama/Meta-Llama-3-8B-Instruct", help="Agent model to use for generating responses")
+    parser.add_argument("--agent_url", type=str, default="c013:1236", help="Agent model url")
     parser.add_argument("--write_file", type=str, default="output.json", help="File to write the output to")
     parser.add_argument("--world_model", type=str, default="meta-llama/Meta-Llama-3-8B-Instruct", help="World model to use for generating responses")
+    parser.add_argument("--debug", type=bool, default=False, help="Debug mode")
+    parser.add_argument("--compare_file_good", type=str, default=None, help="file with higher accuracy")
+    parser.add_argument("--compare_file_bad", type=str, default=None, help="file with lower accuracy")
     args = parser.parse_args()
     heuristic = args.heuristic 
     agent_model = args.agent_model
     world_model = args.world_model
+    agent_url = args.agent_url
+    compare_file_good = args.compare_file_good
+    compare_file_bad = args.compare_file_bad
     write_file = open(args.write_file, 'w')
-        
-    result = apply_async(data_list, heuristic, agent_model, world_model)
+    debug = args.debug
+    
+    if compare_file_good is not None and compare_file_bad is not None:
+        new_data_list = []
+        lines_good, lines_bad = [], []
+        lines_good = open(compare_file_good).readlines()
+        lines_bad = open(compare_file_bad).readlines()
+        for i in range(len(lines_good)):
+            data_good = json.loads(lines_good[i].strip())
+            data_bad = json.loads(lines_bad[i].strip())
+            if data_good['normalized_prediction'] == data_good['normalized_answer'] and data_bad['normalized_prediction'] != data_bad['normalized_answer']:
+                new_data_list.append(data_good['question'])
+        temp_data_list = []
+        for data in data_list:
+            if data["question"] in new_data_list:
+                temp_data_list.append(data)
+        data_list = temp_data_list
+        debug = True
+    
+    if debug:
+        # only test on 10 samples
+        for i in range(10):
+            print("Question: " + data_list[i]['question'])
+            apply_async([data_list[i]], heuristic, agent_model, world_model, agent_url)
+        exit()
+    
+    result = apply_async(data_list, heuristic, agent_model, world_model, agent_url)
     for d in result:
         write_file.write(json.dumps(d) + '\n')
     write_file.close()
