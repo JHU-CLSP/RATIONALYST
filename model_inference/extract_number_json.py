1c3aace250d8c728f56442cd344c072c136233bd
Dongwei <jdw19930725@gmail.com>
2024-06-27 20:35:38 +0000

    modify extract number script

--- model_inference/extract_number_json.py
+++ model_inference/extract_number_json.py
@@ -8,11 +8,13 @@ for i in range(len(cot_lines)):
     total += 1
     d_cot = json.loads(cot_lines[i])
     # if d_cot["normalized_answer"] == d_cot["normalized_prediction"]:
-    if d_cot["answer"] == d_cot["normalized_prediction"]:
+    if d_cot["answer"] == d_cot["normalized_prediction"] or d_cot["normalized_answer"] == d_cot["normalized_prediction"] and d_cot["normalized_answer"] != '':
         correct += 1
-    if  d_cot["normalized_answer"] != "":
+    # else:
+    #     print(d_cot["full_response"])
+    if  d_cot["normalized_prediction"] != "":
         has_answer += 1
         
 print('Correct: ' + str(correct) + ' Has answer: ' + str(has_answer) + ' Total: ' + str(total) + ' Real Total: ' + str(1319))
-print(correct / 1319 * 100)
+print(correct / total * 100)
 
