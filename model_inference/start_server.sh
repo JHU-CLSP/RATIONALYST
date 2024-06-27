f0d316ac993aeaf2f6bfd8909ae63641aec9f49b
Dongwei <jdw19930725@gmail.com>
2024-06-27 20:49:38 +0000

    change the way server start is handled

--- model_inference/start_server.sh
+++
@@ -1,2 +0,0 @@
-# python -m vllm.entrypoints.openai.api_server --model /weka/scratch/djiang21/Dongwei_quiet_star/reasoning_world_model/model_training/output --dtype float32  --api_key "token-abc123" --port 1234 --max-logprobs 1
-python -m vllm.entrypoints.openai.api_server --model /weka/scratch/djiang21/Dongwei_quiet_star/reasoning_world_model/model_training/output --dtype bfloat16  --port 1235 --max-logprobs 1
